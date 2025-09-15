f411a9b7d76bfec1211f0572e50a17ff9e13b49f
Graham Dennis <graham.dennis@gmail.com>
2025-09-15 10:33:22 +1000

    Make it optional to apply git filters

--- src/libfetchers/git.cc
+++ src/libfetchers/git.cc
@@ -178,10 +178,8 @@ struct GitInputScheme : InputScheme
         for (auto & [name, value] : url.query) {
             if (name == "rev" || name == "ref" || name == "keytype" || name == "publicKey" || name == "publicKeys")
                 attrs.emplace(name, value);
-            else if (
-                name == "shallow" || name == "submodules" || name == "lfs" || name == "exportIgnore"
-                || name == "allRefs" || name == "verifyCommit")
-                attrs.emplace(name, Explicit<bool>{value == "1"});
+            else if (name == "shallow" || name == "submodules" || name == "lfs" || name == "exportIgnore" || name == "allRefs" || name == "verifyCommit" || name == "applyFilters")
+                attrs.emplace(name, Explicit<bool> { value == "1" });
             else
                 url2.query.emplace(name, value);
         }
@@ -217,6 +215,7 @@ struct GitInputScheme : InputScheme
             "keytype",
             "publicKey",
             "publicKeys",
+            "applyFilters",
         };
     }
 
@@ -265,6 +264,8 @@ struct GitInputScheme : InputScheme
             url.query.insert_or_assign("publicKey", publicKeys.at(0).key);
         } else if (publicKeys.size() > 1)
             url.query.insert_or_assign("publicKeys", publicKeys_to_string(publicKeys));
+        if (maybeGetBoolAttr(input.attrs, "applyFilters").value_or(false))
+            url.query.insert_or_assign("applyFilters", "1");
         return url;
     }
 
@@ -437,6 +438,11 @@ struct GitInputScheme : InputScheme
     bool getAllRefsAttr(const Input & input) const
     {
         return maybeGetBoolAttr(input.attrs, "allRefs").value_or(false);
+    }
+
+    bool getApplyFiltersAttr(const Input & input) const
+    {
+        return maybeGetBoolAttr(input.attrs, "applyFilters").value_or(false);
     }
 
     RepoInfo getRepoInfo(const Input & input) const
@@ -749,7 +755,8 @@ struct GitInputScheme : InputScheme
 
         bool exportIgnore = getExportIgnoreAttr(input);
         bool smudgeLfs = getLfsAttr(input);
-        auto accessor = repo->getAccessor(rev, exportIgnore, "«" + input.to_string() + "»", smudgeLfs);
+        bool applyFilters = getApplyFiltersAttr(input);
+        auto accessor = repo->getAccessor(rev, exportIgnore, "«" + input.to_string() + "»", smudgeLfs, applyFilters);
 
         /* If the repo has submodules, fetch them and return a mounted
            input accessor consisting of the accessor for the top-level
@@ -780,10 +787,11 @@ struct GitInputScheme : InputScheme
                     }
                 }
                 attrs.insert_or_assign("rev", submoduleRev.gitRev());
-                attrs.insert_or_assign("exportIgnore", Explicit<bool>{exportIgnore});
-                attrs.insert_or_assign("submodules", Explicit<bool>{true});
-                attrs.insert_or_assign("lfs", Explicit<bool>{smudgeLfs});
-                attrs.insert_or_assign("allRefs", Explicit<bool>{true});
+                attrs.insert_or_assign("exportIgnore", Explicit<bool>{ exportIgnore });
+                attrs.insert_or_assign("applyFilters", Explicit<bool>{ applyFilters });
+                attrs.insert_or_assign("submodules", Explicit<bool>{ true });
+                attrs.insert_or_assign("lfs", Explicit<bool>{ smudgeLfs });
+                attrs.insert_or_assign("allRefs", Explicit<bool>{ true });
                 auto submoduleInput = fetchers::Input::fromAttrs(*input.settings, std::move(attrs));
                 auto [submoduleAccessor, submoduleInput2] = submoduleInput.getAccessor(store);
                 submoduleAccessor->setPathDisplay("«" + submoduleInput.to_string() + "»");
@@ -913,9 +921,9 @@ struct GitInputScheme : InputScheme
 
     std::optional<std::string> getFingerprint(ref<Store> store, const Input & input) const override
     {
-        auto makeFingerprint = [&](const Hash & rev) {
-            return rev.gitRev() + (getSubmodulesAttr(input) ? ";s" : "") + (getExportIgnoreAttr(input) ? ";e" : "")
-                   + (getLfsAttr(input) ? ";l" : "");
+        auto makeFingerprint = [&](const Hash & rev)
+        {
+            return rev.gitRev() + (getSubmodulesAttr(input) ? ";s" : "") + (getExportIgnoreAttr(input) ? ";e" : "") + (getLfsAttr(input) ? ";l" : "") + (getApplyFiltersAttr(input) ? ";f" : "");
         };
 
         if (auto rev = input.getRev())
