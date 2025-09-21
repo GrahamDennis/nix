8f32c16f8e83926972d4468014ebf5f41fb667a6
Graham Dennis <gdennis@anduril.com>
2025-09-21 16:31:55 -0700

    Support fallback fetching in legacy mode

--- src/nix/flake.cc
+++ src/nix/flake.cc
@@ -1471,6 +1471,7 @@ struct CmdFlakePrefetch : FlakeCommand, MixJSON
             res["original"] = fetchers::attrsToJSON(resolvedRef.toAttrs());
             res["locked"] = fetchers::attrsToJSON(lockedRef.toAttrs());
             res["locked"].erase("__final"); // internal for now
+            res["locked"].erase("__legacy");
             printJSON(res);
         } else {
             notice(
