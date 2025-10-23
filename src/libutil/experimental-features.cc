9784f338fb54fe48ae83582413da3ae8d1023110
Graham Dennis <gdennis@anduril.com>
2025-10-23 11:40:12 +1100

    Rename experimental feature to no-implicit-final-fetch

--- src/libutil/experimental-features.cc
+++ src/libutil/experimental-features.cc
@@ -24,7 +24,7 @@ struct ExperimentalFeatureDetails
  * feature, we either have no issue at all if few features are not added
  * at the end of the list, or a proper merge conflict if they are.
  */
-constexpr size_t numXpFeatures = 1 + static_cast<size_t>(Xp::DisableImplicitFinalFetch);
+constexpr size_t numXpFeatures = 1 + static_cast<size_t>(Xp::NoImplicitFinalFetch);
 
 constexpr std::array<ExperimentalFeatureDetails, numXpFeatures> xpFeatureDetails = {{
     {
@@ -346,8 +346,8 @@ constexpr std::array<ExperimentalFeatureDetails, numXpFeatures> xpFeatureDetails
         .trackingUrl = "",
     },
     {
-        .tag = Xp::DisableImplicitFinalFetch,
-        .name = "disable-implicit-final-fetch",
+        .tag = Xp::NoImplicitFinalFetch,
+        .name = "no-implicit-final-fetch",
         .description = R"(
             If set to `false` (default), the builtins.fetch* calls will behave as though they are
             "final" if a narHash attribute is supplied.
