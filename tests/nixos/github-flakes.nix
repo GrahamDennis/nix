2d0c5eb01a9d012d6c9f0c4acc38909719a98986
Graham Dennis <gdennis@anduril.com>
2025-10-23 11:48:39 +1100

    Debug test

--- tests/nixos/github-flakes.nix
+++ tests/nixos/github-flakes.nix
@@ -213,7 +213,9 @@ in
       cat_log()
 
       # Fetching with the resolved URL should produce the same result.
-      info2 = json.loads(client.succeed(f"nix flake metadata {info['url']} --json --access-tokens github.com=ghp_000000000000000000000000000000000000 --tarball-ttl 0"))
+      out2 = client.succeed(f"nix flake metadata {info['url']} --json --access-tokens github.com=ghp_000000000000000000000000000000000000 --tarball-ttl 0")
+      print(out2)
+      info2 = json.loads(out2)
       print(info["fingerprint"], info2["fingerprint"])
       assert info["fingerprint"] == info2["fingerprint"], "fingerprint mismatch"
 
