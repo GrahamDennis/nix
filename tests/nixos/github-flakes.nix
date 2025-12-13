2cdbc13acbb8ebb306fafc735ee747682ab6da2f
Graham Dennis <graham.dennis@gmail.com>
2025-12-13 18:39:19 +1100

    Fix incorrect revert

--- tests/nixos/github-flakes.nix
+++ tests/nixos/github-flakes.nix
@@ -213,7 +213,7 @@ in
       cat_log()
 
       # Fetching with the resolved URL should produce the same result.
-      info2 = json.loads(client.succeed(f"nix flake metadata {info['url']} --json --access-tokens github.com=ghp_000000000000000000000000000000000000 --tarball-ttl 0"))
+      info2 = json.loads(client.succeed(f"nix flake metadata {info['url']} --json --access-tokens github.com=ghp_000000000000000000000000000000000000 --tarball-ttl 0 --extra-experimental-features no-implicit-final-fetch"))
       print(info["fingerprint"], info2["fingerprint"])
       assert info["fingerprint"] == info2["fingerprint"], "fingerprint mismatch"
 
